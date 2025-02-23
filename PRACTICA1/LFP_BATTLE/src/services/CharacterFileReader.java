f12c10513ab5f7105931a85e6bb286ecb0f36be4
Tara <98990297+MarioRene@users.noreply.github.com>
2025-02-22 22:38:56 -0600

    Create CharacterFileReader.java

---
+++ PRACTICA1/LFP_BATTLE/src/services/CharacterFileReader.java
@@ -0,0 +1,32 @@
+package services;
+
+import models.Character;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CharacterFileReader {
+
+    public static List<Character> readCharacters(String filePath) {
+        List<Character> characters = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(filePath))) {
+            String line;
+            br.readLine(); // Saltar la primera línea (encabezado)
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                String name = parts[0];
+                int health = Integer.parseInt(parts[1]);
+                int attack = Integer.parseInt(parts[2]);
+                int defense = Integer.parseInt(parts[3]);
+                characters.add(new Character(name, health, attack, defense));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return characters;
+    }
+}
